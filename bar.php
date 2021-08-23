646a92287f5753505e7e6f4b47b1a8fa7aa1ccb5
nomangujjar <89376114+nomangujjar@users.noreply.github.com>
2021-08-23 11:19:45 +0500

    initial commit

---
+++ bar.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<style>
+	ul{
+		padding:  0;
+		margin: 0;
+		list-style-type: none;
+		background-color: azure;
+		overflow: hidden;
+	}
+	li{
+		float: left;
+	}
+	li a{
+		display: block;
+		color: black;
+		text-align: center;
+		text-decoration: none;
+		padding: 10px;
+	}</style>
+</head>
+
+<body>
+<h1>
+	Navigation Bar
+</h1>
+<ul>
+	<li><a href="internal.htm"> HOME </a></li>
+	<li> <a href="#Achivement"> ABOUT </a></li>
+	<li> <a href=" #Contact"> CONTACT </a></li>
+	<li> <a href="#About"> Services </a></li>
+	<li> <a href="#FAQs"> G0al</a></li>
+	<li> <a href="registration.php">   Registration Form </a></li>
+</ul>
+</body>
+</html>
